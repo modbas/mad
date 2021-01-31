e813015e65724aa8db93294cb25bb42b507be410
Frank Traenkle <frank.traenkle@hs-heilbronn.de>
2021-01-31 11:55:25 +0100

    english versions

--- matlab/examples/pt1/pt1_data.m
+++ matlab/examples/pt1/pt1_data.m
@@ -1,17 +1,13 @@
 %%
-%% Modbas- Frank Traenkle - $Id: laplacetable.m 19 2013-08-20 11:06:43Z ftraenkle $
+%% Modbas - Frank Traenkle
 %%
-%% Parameter fuer Simulink-Modell pt1_blockdiagram.slx
+%% Parameters for Simulink model pt1_blockdiagram.slx
 %%
 
-P_R = 100; % Ohmscher Widerstand [ Ohm ];
-P_C = 1e-6; % Kapazitaet [ F ]
+clear variables; % clear workspace
 
-P_T = P_R * P_C; % Zeitkonstante PT1 [ s ]
-P_k = 1; % Verstaerkungsfaktor PT1 [ V / V ]
-P_x0 = 0; % Anfangswert Spannung an Kapazitaet [ V ]
+P_T = 100e-6; % time constant [ s ]
+P_k = 1; % gain [ 1 ]
+P_x0 = 0; % initial value capacity voltage [ V ]
 
-P_u_step = 12; % Versorgungsspannung [ V ]
-P_u_steptime = 1e-3; % Einschaltzeitpunkt [ s ]
-
-P_dt = 1e-5; % Step size ODE solver [ s ]   
+P_dt = 10e-6; % step size ODE solver [ s ]   
--- matlab/examples/pt2/pt2_data.m
+++ matlab/examples/pt2/pt2_data.m
@@ -1,9 +1,20 @@
-%% Simulink Model Parameters
+%%
+%% Modbas - Frank Traenkle
+%%
+%% Parameters for Simulink model pt1_blockdiagram.slx
+%%
 
-P_T = 1e-3; % Time constant of system with no damping [ s ]
-P_D = 0.3; % Damping constant [ 1 ]
-P_k = 1; % Static gain [ 1 ]
-P_x0 = 0; % Initial position [ 1 ]
-P_v0 = 0; % Initial speed [ 1/s ]
-P_u_steptime = 1e-3; % Time of step [ s ]
-P_u_step = 2; % Step [ 1 ]
+clear variables; % clear workspace
+
+P_T = 1e-3; % time constant of system with no damping [ s ]
+P_D = 0.3; % damping constant [ 1 ]
+P_k = 1; % gain [ 1 ]
+P_x0 = 0; % initial position [ 1 ]
+P_v0 = 0; % initial speed [ 1/s ]
+
+% step size computation
+l = roots([ P_T^2 , 2*P_D*P_T , 1 ]); % eigenvalues of PT2
+disp([ 'P_dt must be less than ' ...
+    num2str(0.1 * min(1 / abs(real(l(1))), 2*pi / abs(imag(l(1))))) ...
+    ]);
+P_dt = 1e-4; % chosen step size ODE solver [ s ]
