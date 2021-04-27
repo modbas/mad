2cbd9cb6912f5d7ac8ed8a57d68520b5e56dec21
Frank Traenkle <frank.traenkle@hs-heilbronn.de>
2021-04-27 17:06:39 +0200

    data files formatting

--- matlab/examples/pt1/pt1_data.m
+++ matlab/examples/pt1/pt1_data.m
@@ -7,9 +7,7 @@
 clear variables; % clear workspace
 
 P_T = 100e-6; % time constant [ s ]
-P_k = 1; % gain [ 1 ]
-P_x0 = 0; % initial value capacity voltage [ V ]
-
-P_dt = 10e-6; % step size ODE solver [ s ]   
-
-P_Ta = 5e-6; % sample time of discrete PT1 [ s ]
+P_k = 1;      % gain [ 1 ]
+P_x0 = 0;     % initial value of capacity voltage [ V ]
+P_dt = 10e-6; % step size of ODE solver [ s ]   
+P_Ta = 5e-6;  % sample time of discrete PT1 [ s ]
--- matlab/examples/pt2/pt2_data.m
+++ matlab/examples/pt2/pt2_data.m
@@ -7,15 +7,14 @@
 clear variables; % clear workspace
 
 P_T = 1e-3; % time constant of system with no damping [ s ]
-P_D = 0.3; % damping constant [ 1 ]
-P_k = 1; % gain [ 1 ]
-
-P_x0 = 0; % initial position [ 1 ]
-P_v0 = 0; % initial speed [ 1/s ]
+P_D = 0.3;  % damping constant [ 1 ]
+P_k = 1;    % gain [ 1 ]
+P_x0 = 0;   % initial value [ 1 ]
+P_v0 = 0;   % initial derivative [ 1/s ]
 
 % step size computation
 l = roots([ P_T^2 , 2*P_D*P_T , 1 ]); % eigenvalues of PT2
 disp([ 'P_dt must be less than ' ...
     num2str(0.1 * min(1 / abs(real(l(1))), 2*pi / abs(imag(l(1))))) ...
     ]);
-P_dt = 1e-4; % chosen step size ODE solver [ s ]
+P_dt = 1e-4; % chosen step size of ODE solver [ s ]
